d9f692eced5dc9d33a7708cf4b3aff2288a7fa00
maaalte <mailvonmalte@gmail.com>
2026-03-26 09:40:23 +0100

    bugfix

--- lib/view/components/core/Widgets/activity_widgets/activity_section.dart
+++ lib/view/components/core/Widgets/activity_widgets/activity_section.dart
@@ -11,7 +11,7 @@ class DaySection extends StatelessWidget {
     required this.items,
     required this.day,
   });
-
+// test
   @override
   Widget build(BuildContext context) {
     return Column(
